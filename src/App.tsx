6b8df9c22a895c38f40be40634dd2646c00b285d
Girey <larsvokul@mail.ru>
2025-04-11 16:27:17 +0300

    WSID basic preparation (removing unnecessary files)

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+
+import "./App.scss";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="card">
+      <button onClick={() => setCount((count) => count + 1)}>
+        count is {count}
+      </button>
+    </div>
+  );
+}
+
+export default App;
